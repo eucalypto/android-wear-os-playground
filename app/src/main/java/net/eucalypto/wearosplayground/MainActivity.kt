fb972d46502fe666b4895a500ef3de089244763f
Viktor Papara <viktor@eucalypto.net>
2022-10-17 15:28:16 +0200

    Try to request battery optimization exception from user

--- app/src/main/java/net/eucalypto/wearosplayground/MainActivity.kt
+++ app/src/main/java/net/eucalypto/wearosplayground/MainActivity.kt
@@ -1,10 +1,16 @@
 package net.eucalypto.wearosplayground
 
-import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 import net.eucalypto.wearosplayground.databinding.ActivityMainBinding
+import timber.log.Timber
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,5 +20,24 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+     requestExceptionFromBatteryOptimization()
+
     }
+
+    private fun requestExceptionFromBatteryOptimization() {
+        val powerManager: PowerManager = getSystemService() ?: return
+        logMethodCall()
+        if (powerManager.isIgnoringBatteryOptimizations(packageName).not()) {
+            Timber.d("Asking to be excluded from battery optimization.")
+            val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+            intent.data = Uri.parse("package:${packageName}")
+            startActivity(intent)
+        }
+    }
+}
+
+
+inline fun logMethodCall() {
+    val stackTraceElement = Thread.currentThread().stackTrace[3]
+    Timber.d("${stackTraceElement.methodName}() called")
 }
